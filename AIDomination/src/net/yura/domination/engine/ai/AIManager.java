94b9fa60699271507da8c3fd8f8e1a0fa3c7de55
Michele Piccirillo <michele.piccirillo@gmail.com>
2012-06-03 18:32:30 +0200

    Aggiunto autodiscovery per i file JAR

--- AIDomination/src/net/yura/domination/engine/ai/AIManager.java
+++ AIDomination/src/net/yura/domination/engine/ai/AIManager.java
@@ -2,11 +2,20 @@ package net.yura.domination.engine.ai;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
 
 /**
  * Utilizzato per integrare facilmente nuove AI nel gioco
@@ -15,7 +24,7 @@ import java.util.List;
  *
  */
 public class AIManager {
-	private static HashMap<String, AIClass> AIclasses = new HashMap<String, AIClass>();
+	private static HashMap<String, AIClass> aiClasses = new HashMap<String, AIClass>();
 	
 	/**
 	 * Utilizzato per integrare le Ai nel gioco
@@ -25,7 +34,7 @@ public class AIManager {
 	 * 
 	 */
 	public static void setup(){
-		autodiscovery();
+		autodiscoverAIs();
 	}
 	
 	/**
@@ -33,36 +42,37 @@ public class AIManager {
 	 * 
 	 * @author Michele Piccirillo <michele.piccirillo@gmail.com>
 	 */
-	// TODO Implementa ricerca anche all'interno dei JAR
-	@SuppressWarnings("unchecked")
-	private static void autodiscovery() {
+	private static void autodiscoverAIs() {
 		String classpath = System.getProperty("java.class.path");
 		String[] paths = classpath.split(File.pathSeparator);
-		for(String path : paths) {
-			File dir = new File(path);
-			if(dir.isDirectory()) {
-				List<String> classes = autodiscoveryClasses(dir);
-				String absDir = dir.getAbsolutePath();
-				for(String classFile : classes) {
-					String className = classFile.substring(absDir.length() + 1, classFile.lastIndexOf('.')).replace(File.separatorChar, '.');
-					try {
-						Class<?> cl = Class.forName(className);
-						
-						if(AI.class.isAssignableFrom(cl) && cl.isAnnotationPresent(Discoverable.class)) {
-							System.out.println("Discovered ai " + cl);
-							AIClass clazz = new AIClass((Class<? extends AI>) cl); 
-							AIclasses.put(clazz.getId(), clazz);
-
-						}
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+		
+		List<String> candidateClassNames = new ArrayList<String>();
+		for(String p : paths) {
+			File file = new File(p);
+			if(file.isDirectory()) {
+				candidateClassNames.addAll(autodiscoverDirectory(file, file));
+			} else if(p.toLowerCase().endsWith(".jar")) {
+				candidateClassNames.addAll(autodiscoverJar(file));
+			}
+		}
+		
+		for(String className : candidateClassNames) {
+			try {
+				Class<?> cl = Class.forName(className);
+				
+				if(AI.class.isAssignableFrom(cl) && cl.isAnnotationPresent(Discoverable.class)) {
+					System.out.println("Discovered ai " + cl);
+					AIClass clazz = new AIClass((Class<? extends AI>) cl); 
+					aiClasses.put(clazz.getId(), clazz);
+	
 				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
 	}
 	
-	private static List<String> autodiscoveryClasses(File path) {
+	private static List<String> autodiscoverDirectory(File root, File path) {
 		List<String> classes = new LinkedList<String>();
 		FileFilter classFilter = new FileFilter() {
 			
@@ -77,13 +87,50 @@ public class AIManager {
 		
 		for(File file : path.listFiles(classFilter)) {
 			if(file.isDirectory())
-				classes.addAll(autodiscoveryClasses(file));
-			else
-				classes.add(file.getAbsolutePath());
+				classes.addAll(autodiscoverDirectory(root, file));
+			else {
+				String absRoot = root.getAbsolutePath();
+				String absFile = file.getAbsolutePath();
+				String className = absFile.substring(absRoot.length() + 1, absFile.lastIndexOf('.')).replace(File.separatorChar, '.');
+				int lastDot = className.lastIndexOf('.');
+				String simpleName = (lastDot != -1) ? className.substring(lastDot + 1) : className;
+				if(simpleName.startsWith("AI"))
+					classes.add(className);
+			}
 		}
 		
 		return classes;
 	}
+
+	private static List<String> autodiscoverJar(File file) {
+		JarFile jar = null;
+		try {
+			jar = new JarFile(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return Collections.<String>emptyList();
+		}
+		
+		Enumeration<JarEntry> entries = jar.entries();
+		List<String> classes = new LinkedList<String>();
+		while(entries.hasMoreElements()) {
+			JarEntry entry = entries.nextElement();
+			String name = entry.getName();
+			if(name.endsWith(".class")) {
+				String className = name.substring(0, name.lastIndexOf('.')).replace(File.separatorChar, '.');
+				int lastDot = className.lastIndexOf('.');
+				String simpleName = (lastDot != -1) ? className.substring(lastDot + 1) : className;
+				if(simpleName.startsWith("AI"))
+					classes.add(className);
+			}
+				
+		}
+		
+		return classes;
+	}
+
+
+
 	
 	/**
 	 * Viene utilizzato quando l'engine risolve l'AI a partire dall'id
@@ -91,7 +138,7 @@ public class AIManager {
 	 * @return
 	 */
 	public static Class<? extends AI> getAIClass(String id){
-		return AIclasses.get(id).getAIclass();
+		return aiClasses.get(id).getAIclass();
 	}
 	
 	
@@ -100,7 +147,7 @@ public class AIManager {
 	 * @return
 	 */
 	public static Collection<AIClass> getAIs(){
-		return AIclasses.values();
+		return aiClasses.values();
 	}
 	
 	
