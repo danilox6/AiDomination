1164245aa237fb7853bd16835f3a2b23d5a74160
Danilo Iannelli <daniloiannelli6@gmail.com>
2012-07-17 13:23:27 +0200

    Aggiunto onArmyMove()

--- AIDomination/src/it/unisannio/legiolinteata/ai/AIFuzzy.java
+++ AIDomination/src/it/unisannio/legiolinteata/ai/AIFuzzy.java
@@ -1,5 +1,10 @@
 package it.unisannio.legiolinteata.ai;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -10,7 +15,10 @@ import java.util.concurrent.TimeUnit;
 
 import aima.core.search.adversarial.AdversarialSearch;
 import aima.core.search.adversarial.IterativeDeepeningAlphaBetaSearch;
+import it.unisannio.legiolinteata.advisor.Advisor.Advice;
 import it.unisannio.legiolinteata.advisor.AttackAdvisor;
+import it.unisannio.legiolinteata.advisor.Attacks;
+import it.unisannio.legiolinteata.advisor.ContinentAdvisor;
 import it.unisannio.legiolinteata.advisor.FortificationAdvisor;
 import it.unisannio.legiolinteata.search.GameState;
 import it.unisannio.legiolinteata.search.PlacementAction;
@@ -19,6 +27,9 @@ import net.yura.domination.engine.ai.AIPlayer;
 import net.yura.domination.engine.ai.Discoverable;
 import net.yura.domination.engine.ai.commands.Attack;
 import net.yura.domination.engine.ai.commands.Fortification;
+import net.yura.domination.engine.ai.commands.Move;
+import net.yura.domination.engine.core.AbstractContinent;
+import net.yura.domination.engine.core.AbstractCountry;
 import net.yura.domination.engine.core.Country;
 
 @Discoverable
@@ -67,5 +78,87 @@ public class AIFuzzy extends FallbackAI {
 		} catch (Exception e) {
 			return super.onCountrySelection();
 		} 
+	}
+	
+	@Override
+	@SuppressWarnings({"rawtypes","unchecked"})
+	protected Move onArmyMove() {
+		Vector<Country> ownedCountries = player.getTerritoriesOwned();
+		LinkedList<Country> innerCountries = new LinkedList<Country>();
+		for(Country country: ownedCountries){
+			if(country.getArmies()>1){
+				Vector<Country> neighbours = country.getNeighbours();
+				boolean mine = true;
+				for (Country neighbour: neighbours){
+					if(neighbour.getOwner().getColor() != player.getColor()){
+						mine = false;
+						break;
+					}
+				}
+				if(mine)
+					innerCountries.add(country);
+			}
+		}
+		
+		if(innerCountries.isEmpty())
+			return null;
+		
+		List<MoveUtilityWrapper> moveUtility = new LinkedList<AIFuzzy.MoveUtilityWrapper>();
+		
+		ContinentAdvisor continentAdvisor = new ContinentAdvisor(game, player);
+		List<Advice<AbstractContinent<?,?>>> advices =  continentAdvisor.getAdvices();
+		for (Advice<AbstractContinent<?, ?>> advice : advices) {
+			AbstractContinent continent = advice.getObject();
+			for(AbstractCountry country : innerCountries){
+				int costToContinent = Attacks.getDistanceToContinent(country, continent, player, new LinkedList<AbstractCountry<?,?,?>>(), Integer.MAX_VALUE, 3, false);
+				moveUtility.add(new MoveUtilityWrapper(country, continent, advice.getValue(), costToContinent));
+			}
+		}
+		
+		final MoveUtilityWrapper bestMove = Collections.max(moveUtility);
+		AbstractCountry origin = bestMove.country;
+		
+		Vector<AbstractCountry> neighbours = origin.getNeighbours();
+		
+		AbstractCountry destination = Collections.min(neighbours, new Comparator<AbstractCountry>() {
+
+			@Override
+			public int compare(AbstractCountry o1, AbstractCountry o2) {
+				int o1cost = Attacks.getDistanceToContinent(o1, bestMove.continent, player, new LinkedList<AbstractCountry<?,?,?>>(), Integer.MAX_VALUE, 3, false);
+				int o2cost = Attacks.getDistanceToContinent(o2, bestMove.continent, player, new LinkedList<AbstractCountry<?,?,?>>(), Integer.MAX_VALUE, 3, false);
+				return o1cost-o2cost;
+			}
+		});
+		
+		return new Move(origin, destination, origin.getArmies()-1);
+	}
+	
+	
+	@SuppressWarnings("rawtypes")
+	private class MoveUtilityWrapper implements Comparable<MoveUtilityWrapper>{
+		
+		AbstractCountry country;
+		AbstractContinent continent;
+		double continentAdviceValue;
+		int costToContinent;
+		
+		public MoveUtilityWrapper(AbstractCountry country,	AbstractContinent continent, double continentAdviceValue, int costToContinent) {
+			this.country = country;
+			this.continent = continent;
+			this.continentAdviceValue = continentAdviceValue;
+			this.costToContinent = costToContinent;
+		}
+		
+		public double value(){
+			return country.getArmies() * 1/(costToContinent+1) * continentAdviceValue;
+		}
+
+		@Override
+		public int compareTo(MoveUtilityWrapper o) {
+			return (int) (this.value() - o.value()); //FIXME
+		}
+		
+		
+		
 	}
 }
